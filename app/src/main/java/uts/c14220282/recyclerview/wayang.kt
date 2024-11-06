307be068a646be0f96e0d108b11323ce94dcd424
EdselGitHub <149871435+EdselGitHub@users.noreply.github.com>
2024-11-06 20:26:14 +0700

    Initial commit

---
+++ app/src/main/java/uts/c14220282/recyclerview/wayang.kt
@@ -0,0 +1,10 @@
+package uts.c14220282.recyclerview
+
+class wayang {
+    data class wayang(
+        var foto : String,
+        var nama : String,
+        var karakter : String,
+        var deskripsi : String
+    )
+}
